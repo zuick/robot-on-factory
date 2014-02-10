db8ab60422d86890f41f278cfee49d079fb1af79
ruslan.khakimov <ruslan.khakimov@frumatic.com>
2014-02-10 17:08:49 +0600

    getObjectsFromMap

--- js/game.js
+++ js/game.js
@@ -1,41 +1,38 @@
-var game = new Phaser.Game(640, 320, Phaser.CANVAS, '', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(160, 160, Phaser.CANVAS, '', { preload: preload, create: create, update: update, render: render }, null, false, false);
 
 function preload() {
     
     game.load.spritesheet('hero', 'assets/hero.png', 16, 16);
-    game.load.tilemap('level', 'assets/random.json', null, Phaser.Tilemap.TILED_JSON);
-    game.load.tileset('ground', 'assets/tileset.png', 16, 16);
+    game.load.tilemap('level1', 'assets/level1.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.tilemap('level2', 'assets/level2.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.tileset('tileset', 'assets/tileset.png', 16, 16);
 }
 
 function create() {
     
 
-    map = game.add.tilemap('level');
-    gr = game.add.tileset('ground');
-    gr.setCollision( 2, true, true, true, true );
-    gr.setCollision( 3, true, true, true, true );
-    layer1 = game.add.tilemapLayer(0, 0, 320, 160, gr, map, 0);
-    //game.world.setBounds(0, 0, 480, 320);
+    map = game.add.tilemap('level2');
+    tileset = game.add.tileset('tileset');
+    tileset.setCollision( 4, true, true, true, true );
+    tileset.setCollision( 5, true, true, true, true );
+    layer1 = game.add.tilemapLayer(0, 0, 160, 160, tileset, map, 0);
+    layer1.resizeWorld();
     
-    player = game.add.sprite(0, 0, 'hero');
+    
+    heroXY = getObjectsPositionFromMap( map, 'characters', 9 )[0];
+    player = game.add.sprite( heroXY.x * tileset.tileWidth, heroXY.y * tileset.tileHeight, 'hero');
     
     player.body.bounce.y = 0;
-    player.body.gravity.y = 9;
+    player.body.gravity.y = 18;
     player.body.collideWorldBounds = true;
   
     player.animations.add('left', [0, 1], 7, true);
     player.animations.add('right', [3, 4], 7, true);
-    player.body.setSize(8, 16,7 ,2);
+    player.body.setSize(8, 16, 7 ,2);
     
     cursors = game.input.keyboard.createCursorKeys();
-    
-    Phaser.Canvas.setSmoothingEnabled(game.context, false);
-    
-   
-    layer1.scale.setTo(2,2);
-    player.scale.setTo(2,2);
-    
-    //game.camera.follow( player );
+
+    game.camera.follow( player );
 }
 
 function update() {
@@ -48,7 +45,7 @@ function update() {
     {
         //  Move to the left
         player.body.velocity.x = -100;
- 
+        
         player.animations.play('left');
     }
     else if (cursors.right.isDown)
@@ -76,7 +73,25 @@ function update() {
 
 function render(){
     //game.debug.renderCameraInfo(game.camera, 0, 32);
-    game.debug.renderSpriteBody(player);
-
 }
 
+function getObjectsPositionFromMap( map, layerName, tileIndex ){
+    var result = [];
+    // find layer
+    for( var i in map.layers ){
+        var layer = map.layers[i];
+        if( layer.name == layerName ){
+            // find objects
+            for( var k in layer.data ){
+                for( var l in layer.data ){
+                    if( layer.data[k][l] == tileIndex ){
+                        result.push({x: l , y: k });
+                    }         
+                }
+            }
+            break;
+        }
+    }
+    
+    return result;
+}
